29183ab81f0cd7a965a1b6b072326afe20ecdca0
Jean-Denis Vauguet <jd@vauguet.fr>
2019-10-24 12:16:35 +0200

    Import par défaut pour React-DOM (c'est plus clair)

--- src/index.js
+++ src/index.js
@@ -2,8 +2,16 @@
  * NPM import
  */
 import '@babel/polyfill';
+
+// La librairie 'react' fait un export par défaut, on fait donc un import par
+// défaut (=> on commande le plat du jour)
 import React from 'react';
-import { render } from 'react-dom';
+
+// La librairie 'react-dom' fait plusieurs exports nommés, on fait donc un import
+// nommé en désignant explicitement ce qui nous intéresse (=> on commande à la
+// carte)
+// import { render } from 'react-dom';
+import ReactDOM from 'react-dom';
 
 /**
  * Local import
@@ -15,4 +23,4 @@ import App from 'src/components/App';
  */
 const rootComponent = <App promo="Vortex" />;
 const renderingArea = document.querySelector('#root');
-render(rootComponent, renderingArea);
+ReactDOM.render(rootComponent, renderingArea);
