d610b9b174098890af8062adf717c9fbcd642be3
Jean-Denis Vauguet <jd@vauguet.fr>
2019-10-29 00:44:42 +0100

    Ajout d'un compteur de clics (ex. de stateful component)

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -7,6 +7,7 @@ import React from 'react';
  * Imports locaux
  */
 // Composants React
+import ClickCounter from 'src/components/ClickCounter';
 // Données
 // Styles et assets
 import './app.sass';
@@ -14,13 +15,34 @@ import './app.sass';
 /**
  * Code
  */
-const App = (props) => {
-  const promo = props.promo;
-  return <div id="app">
-    <h1 id="app-title">Modèle React</h1>
-    <p id="app-content"><em>Salut</em> {promo} !</p>
-  </div>;
-};
+class App extends React.Component {
+  state = {
+    click: {
+      count: 0
+    }
+  }
+
+  onCounterClick = () => {
+    this.setState((state) => {
+      const click = {
+        count: state.click.count + 1
+      };
+      return { click };
+    });
+  }
+
+  render() {
+    const promo = this.props.promo;
+    return <div id="app">
+      <h1 id="app-title">Modèle React</h1>
+      <p id="app-content"><em>Salut</em> {promo} !</p>
+      <ClickCounter
+        count={this.state.click.count}
+        onClick={this.onCounterClick}
+      />
+    </div>;
+  }
+}
 
 /**
  * Export
