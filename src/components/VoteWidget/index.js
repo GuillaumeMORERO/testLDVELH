86117ee819a7394efc0380575b75745379c3ba89
Jean-Denis Vauguet <jd@vauguet.fr>
2019-11-06 13:44:43 +0100

    Utilisation du store de l'application pour en modifier l'état

--- src/components/VoteWidget/index.js
+++ src/components/VoteWidget/index.js
@@ -7,10 +7,28 @@ const VoteWidget = () => {
   const state = store.getState();
   const { question } = state;
 
+  const handleYesClick = (event) => {
+    console.log('[VOTE] oui');
+    // comment voter, concrètement ?
+    // Finalité : il faut modifier la valeur de la clé "oui" dans le state qui est
+    // dans le store de l'application.
+    store.dispatch('VOTE_YES');
+    console.log(store.getState());
+  };
+
+  const handleNoClick = (event) => {
+    console.log('[VOTE] non');
+    // comment voter, concrètement ?
+    // Finalité : il faut modifier la valeur de la clé "oui" dans le state qui est
+    // dans le store de l'application.
+    store.dispatch('VOTE_NO');
+    console.log(store.getState());
+  };
+
   return <div className="vote-widget">
     <p>{question}</p>
-    <button>oui</button>
-    <button>non</button>
+    <button onClick={handleYesClick}>oui</button>
+    <button onClick={handleNoClick}>non</button>
   </div>
 };
 
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -5,6 +5,20 @@ const initialState = {
 }
 
 export default (state = initialState, action) => {
-  // TODO: traiter les actions
-  return state;
+  console.log('reducer >>', action);
+
+  switch(action) {
+    case 'VOTE_YES': {
+      // TODO: modifier le state courant (incrémenter le bon compteur)
+      // Attention, il faut travailler sur une copie du state !
+      const newState = { ...state, oui: state.oui + 1 };
+      return newState;
+    }
+    case 'VOTE_NO': {
+      return { ...state, non: state.non + 1 };
+    }
+    default: {
+      return state;
+    }
+  }
 }
