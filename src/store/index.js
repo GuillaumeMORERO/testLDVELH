e00db07059038ae5cc122320a0b91d575de1edc6
Jean-Denis Vauguet <jd@vauguet.fr>
2019-11-08 15:56:44 +0100

    Activation de l'extension Redux DevTools

--- src/store/index.js
+++ src/store/index.js
@@ -6,7 +6,10 @@ import reducer from './reducer';
 
 
 // Création du store de l'application, avec son state privé.
-const reactModelStore = createStore(reducer);
+const reactModelStore = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 // Juste pour debugguer, ne pas laisser en production.
 console.log(reactModelStore);
