569a22f0a33fb88b80b0ab21b8e8782922a295f8
Jean-Denis Vauguet <jd@vauguet.fr>
2019-11-14 19:16:04 +0100

    Exemple de middleware Redux

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,22 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 
 
 // Logique de stockage et de manipulation des données de l'application.
 import reducer from './reducer';
+import mw from './middleware';
 
+
+// applyMiddleware branche les middlewares de l'application au bon endroit
+// sur le trajet des actions qui sont dispatchées vers le store.
+// middlewares est par conséquent un "store enhancer".
+const middlewares = applyMiddleware(mw);
+
+const withReduxDevTools = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 // Création du store de l'application, avec son state privé.
 const reactModelStore = createStore(
   reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  withReduxDevTools(middlewares)
 );
 
 // Juste pour debugguer, ne pas laisser en production.
