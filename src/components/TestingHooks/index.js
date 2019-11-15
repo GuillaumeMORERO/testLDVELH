b986237ab622ca09bcc39455123e21e51458eeb9
Jean-Denis Vauguet <jd@vauguet.fr>
2019-11-15 14:38:09 +0100

    Nouvel outil dans useInput : validation :tada:

--- src/components/TestingHooks/index.js
+++ src/components/TestingHooks/index.js
@@ -1,14 +1,23 @@
 import React from 'react';
+import emailValidator from 'email-validator';
 
 import { useInput } from 'src/hooks';
 
 const TestingHooks = () => {
-  const emailInputTools = useInput('', 'Votre email');
-  const passwordInputTools = useInput('', 'Votre mot de passe');
+  const emailInputTools = useInput('', 'Votre email', {
+    validate: emailValidator.validate
+  });
+  const passwordInputTools = useInput('', 'Votre mot de passe', {
+    validate: (password) => {
+      return password === 'testtest';
+    }
+  });
 
   return <form>
-    <input type='email' {...emailInputTools} />
-    <input type='password' {...passwordInputTools} />
+    <input type='email' {...emailInputTools.props} />
+    <span>{emailInputTools.isValid ? 'OK' : 'KO'}</span>
+    <input type='password' {...passwordInputTools.props} />
+    <span>{passwordInputTools.isValid ? 'OK' : 'KO'}</span>
   </form>;
 };
 
--- src/hooks.js
+++ src/hooks.js
@@ -1,15 +1,32 @@
 import { useState } from 'react';
 
 // useInput(initialValue, placeholder) => value, setValue, isValid, placeholder..
-export const useInput = (initialValue, placeholder) => {
+export const useInput = (initialValue, placeholder, options = {}) => {
   const [value, setValue] = useState(initialValue);
+
+  // useInput a besoin de savoir quelle est la logique de validation à appliquer
+  // à l'input qu'il gère.
+  // L'utilisateur du hook useInput peut (mais ce n'est pas obligatoire) fournir
+  // une fonction de validation, dans options.validate
+  // S'il ne fournit pas de fonction, alors useInput va utiliser une logique de
+  // validation par défaut, qui en gros ne valide rien de spécial.
+  const validate = options.validate || (() => true);
+
+  // On vérifie la validité de la valeur courante du champ.
+  const isValid = validate(value);
 
   // On retourne une boîte à outils spécialisées dans la gestion d'input.
   return {
-    value: value,
-    // setValue: setValue,
-    onChange: (event) => { setValue(event.target.value); },
-    placeholder: placeholder + '…'
+    // props
+    props: {
+      value: value,
+      onChange: (event) => { setValue(event.target.value); },
+      placeholder: placeholder + '…',
+    },
+
+    // outils, informations… à propos de l'input géré par ce hook
+    validate: validate,
+    isValid: isValid
   };
 };
 
