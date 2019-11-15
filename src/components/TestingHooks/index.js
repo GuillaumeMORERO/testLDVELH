100c0cb80daff9bb193ddd785133f82a89cc2453
Jean-Denis Vauguet <jd@vauguet.fr>
2019-11-15 13:08:15 +0100

    Deux champs input gérés par deux useState()

--- src/components/TestingHooks/index.js
+++ src/components/TestingHooks/index.js
@@ -5,13 +5,18 @@ import React, { useState } from 'react';
 // sauvegarder dans un état interne cette valeur, et l'afficher dans l'input.
 const TestingHooks = () => {
   // Nouvelle propriété d'état pour le composant : la valeur de l'input
-  const [value, setValue] = useState('votre email…');
+  const [email, setEmail] = useState('votre email…');
+  const [password, setPassword] = useState('votre mot de passe…');
 
-  const handleChange = (event) => {
-    setValue(event.target.value); // mise-à-jour de la propriété d'état
-  };
+  // const handleChange = (event) => {
+  //   setValue(event.target.value); // mise-à-jour de la propriété d'état
+  // };
 
-  return <input type='text' value={value} onChange={handleChange} />;
+  // Objectif 2 : gérer deux champs inputs
+  return <form>
+    <input type='email' value={email} onChange={e => {setEmail(e.target.value)}} />
+    <input type='password' value={password} onChange={e => {setPassword(e.target.value)}} />
+  </form>;
 };
 
 export default TestingHooks;
