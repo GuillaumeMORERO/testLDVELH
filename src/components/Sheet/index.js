4e6b5282af5335f1fd00e7b3d5e8816cac2e873e
GuillaumeMORERO <boolabitt@hotmail.com>
2020-01-24 18:33:35 +0100

    starting player state

--- src/components/Sheet/index.js
+++ src/components/Sheet/index.js
@@ -2,16 +2,32 @@ import React, { useState } from 'react';
 
 import { Container, OverlayTrigger, Tooltip } from 'react-bootstrap';
 
+import { useSelector, useDispatch } from 'react-redux';
+
 import './style.scss';
 
 export default () => {
+
+  const dispatch = useDispatch();
+  // const {
+  //   nom,
+  //   habileté,
+  //   blindage,
+  //   ptvict,
+  //   avatar
+  // } = useSelector(state => state.player);
+  // console.log(nom, ptvict);
+
+  const player = useSelector(state => state.player);
+  console.log(player);
+  console.log('nom :', player.nom);
 
   return <Container fluid className="sheet">
 
     <div className="pic">
       <img className="pic-arrow_left" src="src/data/bluearrow.png" alt="arrow"/>
       <div className="pic-ture">
-        <img className="pic-ture_head" src="src/data/Aboth.png" alt="Tête de vainqueur"/>
+        <img className="pic-ture_head" src={player.avatar} alt="Tête de vainqueur"/>
       </div>
       <img className="pic-arrow_right" src="src/data/bluearrow.png" alt="arrow"/>
     </div>
@@ -19,7 +35,7 @@ export default () => {
     <div className="hud">
 
       <div className="hud-name">
-        <h1 className="hud-name_perso">Nom du perso</h1>
+        <h1 className="hud-name_perso"> {player.nom} </h1>
       </div>
 
       <div className="separateur-1">
@@ -28,57 +44,27 @@ export default () => {
           <div className="separateur-2">
             <img className="pic-arrow_left-siz" src="src/data/bluearrow.png" alt="arrow"/>
             <OverlayTrigger
-            key="top"
-            placement="top"
-            overlay={
-              <Tooltip id="tooltip-top">
-                Nombre de points d' Habileté' permettant blablabla...
-              </Tooltip>
-            }
-          >
-            <p className="hud-carac_title">Habileté</p>
-          </OverlayTrigger>
+              key="top"
+              placement="top"
+              overlay={
+                <Tooltip id="tooltip-top">
+                  Nombre de points d' Habileté' permettant blablabla...
+                </Tooltip>
+              }
+            >
+              <p className="hud-carac_title">Habileté</p>
+           </OverlayTrigger>
             <img className="pic-arrow_right-siz" src="src/data/bluearrow.png" alt="arrow"/>
           </div>
           <div className="hud-carac_display">
-           nombre de points d' habileté
+           Nombre de dé(s) : 
+            <div className="hud-carac_display-cadre">
+              {player.habileté}
+            </div>
           </div>
         </div>
 
-        <div className="hud-carac">
-        <OverlayTrigger
-          key="top"
-          placement="top"
-          overlay={
-            <Tooltip id="tooltip-top">
-              Nombre de points de blindage permettant blablabla...
-            </Tooltip>
-          }
-        >
-          <p className="hud-carac_title">Blindage</p>
-        </OverlayTrigger>
-        <div className="hud-carac_display">
-          quantité de blindage
-        </div>
-      </div>
-      
-        <div className="hud-carac">
-        <OverlayTrigger
-          key="top"
-          placement="top"
-          overlay={
-            <Tooltip id="tooltip-top">
-              Nombre de points de victoire permettant blablabla...
-            </Tooltip>
-          }
-        >
-          <p className="hud-carac_title">Points de victoires</p>
-        </OverlayTrigger>
-        <div className="hud-carac_display">
-          nombre de points de victoire
-        </div>
-      </div>
-
+        
       </div>
 
     </div>
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 
 import votes from 'src/store/votes';
+import player from 'src/store/player/reducer';
 
 export default combineReducers({
-  votes
+  votes,
+  player
 });
