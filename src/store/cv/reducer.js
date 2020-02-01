e0b8724ca2a38b70759c644e578b2faffff20028
GuillaumeMORERO <boolabitt@hotmail.com>
2020-02-01 17:04:15 +0100

    dynamic reducer

--- src/store/cv/reducer.js
+++ src/store/cv/reducer.js
@@ -11,23 +11,8 @@ const initialState = {
 
 export default (state = initialState, action) => {
   switch (action.value) {
-    case 1: {
-      return { ...state, 1: true };
-    }
-    case 2: {
-      return { ...state, 2: true };
-    }
-    case 3: {
-      return { ...state, 3: true };
-    }
-    case 4: {
-      return { ...state, 4: true };
-    }
-    case 5: {
-      return { ...state, 5: true };
-    }
-    case 6: {
-      return { ...state, 6: true };
+    case action.value: {
+      return { ...state, [action.value]: true };
     }
     default: {
       return state;
